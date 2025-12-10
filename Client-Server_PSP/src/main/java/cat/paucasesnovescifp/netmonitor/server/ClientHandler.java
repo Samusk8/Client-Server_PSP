14e11ad2daf48c4119f4591085814310d201531d
Samuel <samueljimenez@paucasesnovescifp.cat>
2025-12-10 21:15:09 +0100

    aceptar clientes
    aceptar clientes y crear un client handler

--- Client-Server_PSP/src/main/java/cat/paucasesnovescifp/netmonitor/server/ClientHandler.java
+++ Client-Server_PSP/src/main/java/cat/paucasesnovescifp/netmonitor/server/ClientHandler.java
@@ -1,4 +1,17 @@
 package cat.paucasesnovescifp.netmonitor.server;
 
-public class ClientHandler {
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+    private Socket client;
+
+    public ClientHandler(Socket client) {
+        this.client = client;
+    }
+
+
+    @Override
+    public void run() {
+        System.out.println("Client handler inisiau");
+    }
 }
--- Client-Server_PSP/src/main/java/cat/paucasesnovescifp/netmonitor/server/TCPServer.java
+++ Client-Server_PSP/src/main/java/cat/paucasesnovescifp/netmonitor/server/TCPServer.java
@@ -1,4 +1,21 @@
 package cat.paucasesnovescifp.netmonitor.server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TCPServer {
+    public static int port = 5000;
+
+    static void main() {
+        try (ServerSocket server = new ServerSocket(port)){
+            while (true) {
+                Socket client = server.accept();
+
+                ClientHandler ch = new ClientHandler(client);
+                new Thread(ch).start();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
