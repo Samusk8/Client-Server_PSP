b1f0d49e3e84840303c5d97c74570a1a73d71b8c
Samuel <samueljimenez@paucasesnovescifp.cat>
2025-12-10 21:18:25 +0100

    client info getter and setter

--- Client-Server_PSP/src/main/java/cat/paucasesnovescifp/netmonitor/server/ClientInfo.java
+++ Client-Server_PSP/src/main/java/cat/paucasesnovescifp/netmonitor/server/ClientInfo.java
@@ -5,4 +5,33 @@ import java.net.InetAddress;
 public class ClientInfo {
     InetAddress address;
     int udpPort;
+
+    public ClientInfo(InetAddress address,  int udpPort) {
+        this.address = address;
+        this.udpPort = udpPort;
+    }
+
+    public InetAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(InetAddress address) {
+        this.address = address;
+    }
+
+    public int getUdpPort() {
+        return udpPort;
+    }
+
+    public void setUdpPort(int udpPort) {
+        this.udpPort = udpPort;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientInfo{" +
+                "address=" + address +
+                ", udpPort=" + udpPort +
+                '}';
+    }
 }
