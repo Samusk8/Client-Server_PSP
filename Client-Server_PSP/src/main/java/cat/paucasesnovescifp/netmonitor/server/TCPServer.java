2557d7167ba4918a4e9d8ea8fdc0fd7282517d2e
Samuel <samueljimenez@paucasesnovescifp.cat>
2025-12-10 22:35:19 +0100

    add client to clients

--- Client-Server_PSP/src/main/java/cat/paucasesnovescifp/netmonitor/server/TCPServer.java
+++ Client-Server_PSP/src/main/java/cat/paucasesnovescifp/netmonitor/server/TCPServer.java
@@ -2,9 +2,13 @@ package cat.paucasesnovescifp.netmonitor.server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TCPServer {
     public static int port = 5000;
+    public static List<ClientHandler> handlers = new ArrayList<>();
+    public static List<ClientInfo> clientes = new ArrayList<>();
 
     static void main() {
         try (ServerSocket server = new ServerSocket(port)){
@@ -12,10 +16,31 @@ public class TCPServer {
                 Socket client = server.accept();
 
                 ClientHandler ch = new ClientHandler(client);
-                new Thread(ch).start();
+                handlers.add(ch);
+
+                Thread hilo = new Thread(ch);
+                hilo.start();
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
+    public static void addClient(ClientInfo c){
+        synchronized (clientes){
+            boolean existe = false;
+            for (ClientInfo cc : clientes) {
+                if (cc.getAddress().equals(c.getAddress())) {
+                    existe = true;
+                    System.out.println("El cliente ya existe");
+                    break;
+                }
+                if (!existe) {
+                    clientes.add(c);
+                    System.out.println("Cliente "+c+" añadido");
+                }
+            }
+        }
+    }
+
+    public static void removeClient(ClientInfo c){}
 }
